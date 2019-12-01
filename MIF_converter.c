c48692af7af4f5aa4d952cb3e56fe91653ac7787
kevinchang95 <kevinchanguh@gmail.com>
2019-12-01 14:00:03 -0600

    MIF converter copy correct

--- MIF_converter.c
+++ MIF_converter.c
@@ -7,8 +7,9 @@ int main(int argc, char* argv[])
     //har const* const fileName = argv[1]; /* should check that argc > 1 */
     
 	char fileName[] = "input.coe";
+	//char fileName[] = "test.txt";
 	char MIFName[]  = "MIF.txt";
-	FILE* file = fopen(fileName, "r"); /* should check the result */
+	FILE* file = fopen(fileName, "rb"); /* should check the result */
     FILE* MIF  = fopen(MIFName,"w+");
 	char line[256];
 	int i = 0;
@@ -27,54 +28,72 @@ int main(int argc, char* argv[])
 	char cat[100];
 	char *pos;
 	
-	//while (!feof(file))
-		for(int j = 0; j < 4; j++){
+	while (!feof(file)){
+		//for(int j = 0; j < 4; j++){
         
-		
-		fgets(line, sizeof(line), file);
+		if(fgets(line, sizeof(line), file) != NULL){				//Caution: If EOF is encountered, the str remain unchanged!
+																	//But the return value will be NULL.
 		i++;
 		if(i<3)continue;
-		/*printf("original: %s",line);
-		if ((pos=strchr(line, '\n')) != NULL) {
-			printf("true!");
-			* pos = '\0';
-		}
-		printf("real:%s",line);*/
-    /* input too long for buffer, flag error */
-		
-       // printf("%s", line); 
+			
 		strcpy(cat,zeros);
 		strcat(cat,line);
-		/*if ((pos=strchr(cat, '\n')) != NULL) {
-			//printf("true!");
-			* pos = '\0';
-		}
-		*/
 		
 		//printf("%c\n",cat[strlen(cat)-3]);
 		cat[strlen(cat)-2]='\0';
 		strcat(cat,";\n");
-		printf("%s",cat);
+		//printf("%s",cat);
 		fputs(cat,MIF);
 		
+		}
 		
 		
     }
 	
 	fputs("END",MIF);
-    printf("%d",sizeof(line));
+    //printf("%d",sizeof(line));
 	
-	
-	
-	
-	
+
 	fclose(file);
 	fclose(MIF);
     
 	
 	
-	
-	
-	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*printf("original: %s",line);
+	if ((pos=strchr(line, '\n')) != NULL) {
+		printf("true!");
+		* pos = '\0';
+	}
+	printf("real:%s",line);*/
+/* input too long for buffer, flag error */
+	
+   // printf("%s", line); 
+	   
+	   
+	   
+	   
+
+/*if ((pos=strchr(cat, '\n')) != NULL) {
+//printf("true!");
+* pos = '\0';
+}
+*/
